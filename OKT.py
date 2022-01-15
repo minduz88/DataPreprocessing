336b75a1efe99377dd071d21049f1eaf52192873
Mingyu Kim <minduz88@gmail.com>
2022-01-16 06:06:05 +0900

    update

--- OKT.py
+++ OKT.py
@@ -1,7 +1,13 @@
-from konlpy import Okt
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
 
-text = "챗봇 프로젝트 형태소 분석기 테스트"
+from konlpy.tag import Twitter 
+from konlpy.tag import Okt 
+twitter = Twitter() 
+okt = Okt() 
+print(twitter.morphs(u'단독입찰보다 복수입찰의 경우')) 
+print(okt.morphs(u'단독입찰보다 복수입창의 경우'))
 
-Okt = Okt()
-test = Okt.nous(text)
-print(test)
+
